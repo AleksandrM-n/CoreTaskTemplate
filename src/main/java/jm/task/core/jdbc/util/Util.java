3b81f5eb431659a35a314a912c4307c37f276f11
AleksandrM-n <maklay_bam@mail.ru>
2021-04-16 09:39:14 +0300

    my first commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,15 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() throws SQLException {
+        Driver driver = new FabricMySQLDriver();
+        DriverManager.registerDriver(driver);
+
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/somedb?useSSL=false",
+                "root", "959297");
+    }
 }
